9fca7c50061b8e077c86321c24df4564e3e6832e
morgothqwe <morgothqwe@gmail.com>
2025-06-04 14:23:45 +0330

    Initial Release

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,14 +2,11 @@ import * as model from "./model";
 import view from "./view";
 
 const controlSelectCity = async function (city) {
-  // const cityName = view.addHandlerSelectCity(city);
-  // view.renderWeather(cityName);
   const data = await model.weatherCondition(city);
   view.renderWeather(data);
 };
 
 const init = async function () {
-  // await model.weatherCondition(controlSelectCity);
   view.addHandlerSelectCity(controlSelectCity);
   controlSelectCity("Isfahan");
 };
