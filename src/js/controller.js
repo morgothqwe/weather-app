78dd0e1922bd73dfea51c4c770c2455d192068c0
morgothqwe <morgothqwe@gmail.com>
2025-06-06 09:07:12 +0330

    Error Handling

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,9 +2,13 @@ import * as model from "./model";
 import view from "./view";
 
 const controlSelectCity = async function (city) {
-  view.renderSpinner();
-  const data = await model.weatherCondition(city);
-  view.renderWeather(data);
+  try {
+    view.renderSpinner();
+    const data = await model.weatherCondition(city);
+    view.renderWeather(data);
+  } catch (err) {
+    console.log(`${err}: Please Try Again!`);
+  }
 };
 
 const init = function () {
--- src/js/model.js
+++ src/js/model.js
@@ -1,18 +1,21 @@
 import { API_KEY } from "./config";
 
 export const weatherCondition = async function (city) {
-  const geoResponse = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-  );
-  const geoData = await geoResponse.json();
-  console.log(geoData);
+  try {
+    const geoResponse = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    const geoData = await geoResponse.json();
 
-  return {
-    temperature: geoData.main.temp,
-    humidity: geoData.main.humidity,
-    pressure: geoData.main.pressure,
-    wind: geoData.wind.speed,
-    weatherCondition: geoData.weather[0].main,
-    name: geoData.name,
-  };
+    return {
+      temperature: geoData.main.temp,
+      humidity: geoData.main.humidity,
+      pressure: geoData.main.pressure,
+      wind: geoData.wind.speed,
+      weatherCondition: geoData.weather[0].main,
+      name: geoData.name,
+    };
+  } catch (error) {
+    console.log(`${error}: Please try again!`);
+  }
 };
